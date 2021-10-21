695d302270cb91169ee09c37e91b999ba27bded5
Grovre <50428844+Grovre@users.noreply.github.com>
2021-10-21 13:52:40 -0500

    Fractions class methods completed and working

--- src/com/company/Fractions.java
+++ src/com/company/Fractions.java
@@ -13,10 +13,7 @@ public class Fractions {
         denom = denominator;
         // Makes sure that any two negatives will return a positive and the negative on the denominator moves to the numerator
         this.correctNegatives();
-        sqrt();
         System.out.println("New fraction created: " + this);
-        System.out.println("Fraction √numerator: " + numerRoot[0] + "√" + numerRoot[1]);
-        System.out.println("Fraction √denominator: " + denomRoot[0] + "√" + denomRoot[1]);
     }
 
     // Returns the numerator of the fraction object
@@ -49,28 +46,18 @@ public class Fractions {
 
     // Used to add fractions, lcd calculated and all. Lcm is for the first fraction
     public Fractions addFractions(Fractions fraction) {
-        // System.out.println("\n\n\nAdding Fractions");
-        // assigning numer1 and denom1 keeps the original fraction
+        System.out.println("\n\n\nAdding Fractions");
+        // assigning numer1 and denom1 keeps the original fraction, and correcting the negatives lets
+        correctNegatives();
+        fraction.correctNegatives();
         int numer1 = numer;
         int denom1 = denom;
         int numer2 = fraction.returnNumerator();
         int denom2 = fraction.returnDenominator();
+        int smallerDenom = Math.min(denom1, denom2);
+        int largerDenom = Math.max(denom1, denom2);
 
-        // While loop searches for lcm. Works by setting lcd to any denominator (set to calling object's denom) * lcm. It then checks if the smaller denominator is a factor of the new lcd. If not, increase the lcm by 1 and repeat until one is found.
-        boolean noLCD = true;
-        int lcm = 1;
-        int smallerDenom = Math.min(Math.abs(denom1), Math.abs(denom2));
-        int largerDenom = Math.max(Math.abs(denom1), Math.abs(denom2));
-        int lcd = 0;
-        while (noLCD) {
-            lcd = largerDenom * lcm;
-            if (((double) lcd / smallerDenom) % 1 != 0) {
-                lcm++;
-            } else {
-                noLCD = false;
-            }
-        }
-
+        int lcd = getLCD(fraction);
         // Just makes sure the program doesn't die by killing it before it can die a painful death...
         if(lcd == 0) {
             System.out.println("LCD still 0");
@@ -78,66 +65,35 @@ public class Fractions {
         }
 
         // Assigns all the fraction number used to what they should be when setting up a problem like this on paper
-        System.out.println("\nAdding...\nFraction 1: " + this + "\nFraction 2: " + fraction);
-        if(smallerDenom == denom1 && smallerDenom != denom2) {
-            System.out.println("numer1 before: " + numer1);
-            numer1 *= lcm*2;
-            System.out.println("numer1 after: " + numer1);
-            System.out.println("denom1 before: " + denom1);
-            denom1 *= lcm*2;
-            System.out.println("denom1 after: " + denom1);
-            System.out.println("numer2 before: " + numer2);
-            numer2 *= lcd / denom2;
-            System.out.println("numer2 after: " + numer2);
-            System.out.println("denom2 before: " + denom2);
-            denom2 *= lcd / denom2;
-            System.out.println("denom2 after: " + denom2);
-        } else if(smallerDenom != denom1 && smallerDenom == denom2) {
-            System.out.println("numer2 before: " + numer2);
-            numer2 *= lcm;
-            System.out.println("numer2 after: " + numer2);
-            System.out.println("denom2 before: " + denom2);
-            denom2 *= lcm;
-            System.out.println("denom2 after: " + denom2);
-            System.out.println("numer1 before: " + numer1);
-            numer1 *= lcd / denom1;
-            System.out.println("numer1 after: " + numer1);
-            System.out.println("denom1 before: " + denom1);
-            denom1 *= lcd / denom1;
-            System.out.println("denom1 after: " + denom1);
-        }
+        System.out.println("Factors: " + (lcd / denom) + " x " + (lcd / denom2));
+        denom1 *= (lcd / denom);
+        numer1 *= (lcd / denom);
+        int temp = denom2;
+        denom2 *= (lcd / denom2);
+        numer2 *= (lcd / temp);
 
-        System.out.println("Denominators match (" + denom1 + ", " + denom2 + "): " + (denom1 == denom2));
+        System.out.println("LCD fractions: ");
+        System.out.println("Fraction 1: " +numer1 + "/" + denom1 + "\nFraction 2: " + numer2 + "/" + denom2);
 
-        int gcf = Simplify.simplifyAll(numer1 + numer2, denom1);
-
-        return new Fractions((numer1 += numer2)/gcf, denom1/gcf);
+        Fractions newFraction = new Fractions((numer1 + numer2), denom1);
+        newFraction = newFraction.simplifyFraction();
+        return newFraction;
     }
 
     // Used to subtract fractions, lcd calculated and all. Lcm is for the first fraction
     public Fractions subtractFractions(Fractions fraction) {
-        // System.out.println("\n\n\nSubtracting Fractions");
-        // assigning numer1 and denom1 keeps the original fraction
+        System.out.println("\n\n\nSubtracting Fractions");
+        // assigning numer1 and denom1 keeps the original fraction, and correcting the negatives lets
+        correctNegatives();
+        fraction.correctNegatives();
         int numer1 = numer;
         int denom1 = denom;
         int numer2 = fraction.returnNumerator();
         int denom2 = fraction.returnDenominator();
+        int smallerDenom = Math.min(denom1, denom2);
+        int largerDenom = Math.max(denom1, denom2);
 
-        // While loop searches for lcm. Works by setting lcd to any denominator (set to calling object's denom) * lcm. It then checks if the smaller denominator is a factor of the new lcd. If not, increase the lcm by 1 and repeat until one is found.
-        boolean noLCD = true;
-        int lcm = 1;
-        int smallerDenom = Math.min(Math.abs(denom1), Math.abs(denom2));
-        int largerDenom = Math.max(Math.abs(denom1), Math.abs(denom2));
-        int lcd = 0;
-        while (noLCD) {
-            lcd = largerDenom * lcm;
-            if (((double) lcd / smallerDenom) % 1 != 0) {
-                lcm++;
-            } else {
-                noLCD = false;
-            }
-        }
-
+        int lcd = getLCD(fraction);
         // Just makes sure the program doesn't die by killing it before it can die a painful death...
         if(lcd == 0) {
             System.out.println("LCD still 0");
@@ -145,18 +101,22 @@ public class Fractions {
         }
 
         // Assigns all the fraction number used to what they should be when setting up a problem like this on paper
-        numer1 *= lcm;
-        denom1 *= lcm;
-        numer2 *= lcd / smallerDenom;
-        denom2 *= lcd / smallerDenom;
-        int gcf = Simplify.simplifyAll(numer1 - numer2, denom1);
+        denom1 *= (lcd / denom);
+        numer1 *= (lcd / denom);
+        int temp = denom2;
+        denom2 *= (lcd / denom2);
+        numer2 *= (lcd / temp);
+        System.out.println("LCD fractions: ");
+        System.out.println("Fraction 1: " +numer1 + "/" + denom1 + "\nFraction 2: " + numer2 + "/" + denom2);
 
-        return new Fractions((numer1 -= numer2)/gcf, denom1/gcf);
+        Fractions newFraction = new Fractions((numer1 - numer2), denom1);
+        newFraction = newFraction.simplifyFraction();
+        return newFraction;
     }
 
     // Used to multiply fractions
     public Fractions multiplyFractions(Fractions fraction) {
-        // System.out.println("\n\n\nMultiplying Fractions");
+        System.out.println("\n\n\nMultiplying Fractions");
         int numer2 = fraction.returnNumerator();
         int denom2 = fraction.returnDenominator();
         int gcf = Simplify.simplifyAll(numer*numer2, denom2*denom);
@@ -165,7 +125,7 @@ public class Fractions {
 
     // Used to divide fractions, reciprocal and all
     public Fractions divideFractions(Fractions fraction) {
-        // System.out.println("\n\n\n Dividing Fractions");
+        System.out.println("\n\n\n Dividing Fractions");
         // Gets reciprocal by assigning numer and denom to their opposites 😈
         int denom2 = fraction.returnNumerator();
         int numer2 = fraction.returnDenominator();
@@ -200,6 +160,25 @@ public class Fractions {
 
     public int[] returnDenomRoot() {
         return denomRoot;
+    }
+
+    // Returns the lCD
+    public int getLCD(Fractions fraction2) {
+        int denom1 = denom;
+        int denom2 = fraction2.returnDenominator();
+        int smallerDenom = Math.min(denom1, denom2);
+        int largerDenom = Math.max(denom1, denom2);
+        int i = largerDenom;
+        int lcd;
+        while(true) {
+            if(i % largerDenom == 0 && i % smallerDenom == 0) {
+                lcd = i;
+                System.out.println("LCD found: " + lcd);
+                return lcd;
+            } else {
+                i++;
+            }
+        }
     }
 
 }
--- src/com/company/test.java
+++ src/com/company/test.java
@@ -3,22 +3,17 @@ package com.company;
 public class test {
     public static void main(String[] args) {
         // Fractions test
-        /* Fractions fraction1 = new Fractions(6, 8);
-        Fractions fraction2 = new Fractions(2, 4);
-
-        System.out.println(fraction1);
-        System.out.println(fraction2);
+         Fractions fraction1 = new Fractions(7, 8);
+        Fractions fraction2 = new Fractions(10, 9);
 
 
         System.out.println("Addition: " + fraction1 + " + " + fraction2 + " = " + fraction1.addFractions(fraction2) + "\n\n");
         System.out.println("Subtraction: " + fraction1 + " - " + fraction2 + " = " + fraction1.subtractFractions(fraction2) + "\n\n");
         System.out.println("Multiplication: " + fraction1 + " * " + fraction2 + " = " + fraction1.multiplyFractions(fraction2) + "\n\n");
-        System.out.println("Division: " + fraction1 + " / " + fraction2 + " = " + fraction1.divideFractions(fraction2) + "\n\n"); */
+        System.out.println("Division: " + fraction1 + " / " + fraction2 + " = " + fraction1.divideFractions(fraction2) + "\n\n");
+
 
         // CompleteSquare test
-        int a = 6;
-        int b = 9;
-        int c = 4;
-        new CompleteSquare(a, b, c);
+        // new CompleteSquare(1, -4, 15);
     }
 }
